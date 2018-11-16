f0349c1c5bc0ae34270b1e4d9d7477c5255dbb8d
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2018-11-16 00:55:56 -0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,31 @@
+document.write('\<link rel="stylesheet" href="css/nav.css">\
+	<nav id="nav">\
+			<h1>Marjorie Ann Cuerdo</h1>\
+			<a href="index.html">ABOUT</a>\
+			<a href="projects.html">PROJECTS</a>\
+			<a href="resume.html">RESUME</a>\
+			<a href="blog.html">BLOG</a>\
+		</nav>\
+		<section id="about" name="about">\
+			<div class="about">\
+				<div class="container">\
+					<div class="row">\
+							<div class="col-lg-6">\
+								<p><img src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
+								<p>M.S Human-Computer Interaction<br>DePaul University</p>\
+							</div>\
+							<div class="col-lg-6">\
+								<p>Email: <a href="mailto:mcuerdo@mail.depaul.edu">mcuerdo@mail.depaul.edu</a><br></p>\
+							</div>\
+							<div id="links">\
+								<a href="https://www.linkedin.com/in/marjorie-ann-cuerdo-12979277/" target="_blank"><img src="images/linkedin.png" ></a>\
+								<a href="https://twitter.com/marj_cuerdo" target="_blank"><img src="images/twitter.png"></a>\
+								<a href="https://github.com/mcuerdo" target="_blank"><img src="images/github.png"></a>\
+							</div>\
+						</div>\
+					</div>\
+				</div>\
+				<a class="twitter-timeline" href="https://twitter.com/marj_cuerdo?ref_src=twsrc%5Etfw">Tweets by marj_cuerdo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
+			</div>\
+		</section>');
+
