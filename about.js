191542257c97d7e0564f757a6305029cf9ffc8b5
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2019-02-07 16:16:50 -0500

    Adjusted mobile ordering view of content

---
+++ about.js
@@ -0,0 +1,22 @@
+document.write('<section id="about" name="about">\
+			<div class="about">\
+				<div class="container">\
+					<div class="row">\
+							<div class="col-lg-6">\
+								<p><img src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
+								<p>M.S Human-Computer Interaction<br>DePaul University</p>\
+							</div>\
+							<div class="col-lg-6">\
+								<p>Email: <a href="mailto:mcuerdo@mail.depaul.edu">mcuerdo@mail.depaul.edu</a><br></p>\
+							</div>\
+							<div id="links">\
+								<a href="https://www.linkedin.com/in/marjorie-ann-cuerdo-12979277/" target="_blank"><img src="images/linkedin.png" ></a>\
+								<a href="https://twitter.com/marj_cuerdo" target="_blank"><img src="images/twitter.png"></a>\
+								<a href="https://github.com/marjcuerdo" target="_blank"><img src="images/github.svg"></a>\
+							</div>\
+						</div>\
+					</div>\
+				</div>\
+				<a class="twitter-timeline" href="https://twitter.com/marj_cuerdo?ref_src=twsrc%5Etfw">Tweets by marj_cuerdo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
+			</div>\
+		</section>');
--- nav.js
+++ nav.js
@@ -12,31 +12,23 @@ document.write('\<link rel="stylesheet" href="css/nav.css"><link rel="stylesheet
 				<a href="resume.html">RESUME</a>\
 				<a href="blog.html">BLOG</a>\
 			</div>\
-    </div>\
-		<section id="about" name="about">\
-			<div class="about">\
-				<div class="container">\
-					<div class="row">\
-							<div class="col-lg-6">\
-								<p><img src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
-								<p>M.S Human-Computer Interaction<br>DePaul University</p>\
-							</div>\
-							<div class="col-lg-6">\
-								<p>Email: <a href="mailto:mcuerdo@mail.depaul.edu">mcuerdo@mail.depaul.edu</a><br></p>\
-							</div>\
-							<div id="links">\
-								<a href="https://www.linkedin.com/in/marjorie-ann-cuerdo-12979277/" target="_blank"><img src="images/linkedin.png" ></a>\
-								<a href="https://twitter.com/marj_cuerdo" target="_blank"><img src="images/twitter.png"></a>\
-								<a href="https://github.com/marjcuerdo" target="_blank"><img src="images/github.svg"></a>\
-							</div>\
-						</div>\
-					</div>\
-				</div>\
-				<a class="twitter-timeline" href="https://twitter.com/marj_cuerdo?ref_src=twsrc%5Etfw">Tweets by marj_cuerdo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
-			</div>\
-		</section>');
+    </div>');
 
+$(document).ready(function(){
+   $(window).on('resize' , function(){
+            resize('#about' , '#bio');
+   });
 
+});
+
+function resize(el1 , el2){
+    var $window = $(window);
+    if($window.width() <= 600){
+         $(el2).insertBefore(el1);
+    }else{
+         $(el2).insertAfter(el1);
+    }
+}
 
 function myFunction() {
   var x = document.getElementById("myLinks");
