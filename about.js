ff9c26637c0ddde9e5b3f20cc28f34ea3399657c
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2020-04-17 20:24:49 -0500

    Updated overall website

--- about.js
+++ about.js
@@ -3,7 +3,6 @@ document.write('<section id="about" name="about">\
 				<div class="container">\
 					<div class="row">\
 							<div class="col-lg-6">\
-								<p><strong>About Me</strong></p>\
 								<p><img id="pic" src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
 								<p>M.S. Human-Computer Interaction</p>\
 							</div>\
