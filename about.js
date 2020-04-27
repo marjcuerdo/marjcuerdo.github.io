79a228efa837532b2ff2ee3e027af2276b151145
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2020-04-27 01:05:56 -0500

    Updated view sizing and nav appearance

--- about.js
+++ about.js
@@ -3,20 +3,22 @@ document.write('<section id="about" name="about">\
 				<div class="container">\
 					<div class="row">\
 							<div class="col-lg-6">\
-								<p><img id="pic" src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
-								<p>M.S. Human-Computer Interaction</p>\
+								<b><p><img id="pic" src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
+								<p>M.S. Human-Computer Interaction</p></b>\
 							</div>\
 							<div class="col-lg-6">\
 								<p><a href="mailto:mcuerdo@depaul.edu">mcuerdo@depaul.edu</a><br></p>\
 							</div>\
 							<div id="links">\
+								<a href="https://scholar.google.com/citations?user=nvgtgq4AAAAJ&hl=en" target="_blank"><img src="images/scholar.jpeg"></a>\
+								<a href="https://www.linkedin.com/in/marjorie-ann-cuerdo-12979277/" target="_blank"><img src="images/linkedin.png" ></a>\
 								<a href="https://twitter.com/marj_cuerdo" target="_blank"><img src="images/twitter.png"></a>\
 								<a href="https://github.com/marjcuerdo" target="_blank"><img src="images/github.svg"></a>\
-								<a href="https://www.linkedin.com/in/marjorie-ann-cuerdo-12979277/" target="_blank"><img src="images/linkedin.png" ></a>\
 							</div>\
-						</div>\
 					</div>\
 				</div>\
-				<a class="twitter-timeline" href="https://twitter.com/marj_cuerdo?ref_src=twsrc%5Etfw">Tweets by marj_cuerdo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
 			</div>\
+				<div class="twit">\
+					<a class="twitter-timeline" href="https://twitter.com/marj_cuerdo?ref_src=twsrc%5Etfw">Tweets by marj_cuerdo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
+				</div>\
 		</section>');
--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 document.write('\<link rel="stylesheet" href="css/nav.css"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
 	<div class="topnav" id="myTopnav">\
 		<div class="topbar">\
-		<h1>Marjorie Ann Cuerdo <p class="subtitle">HCI+Games UX</p></h1>\
+		<h1>Marjorie Ann Cuerdo <p class="subtitle">HCI + Games UX</p></h1>\
 				<a href="javascript:void(0);" id="icon" onclick="myFunction()">\
 	    			<i class="fa fa-bars"></i>\
 	    		</a>\
@@ -15,21 +15,7 @@ document.write('\<link rel="stylesheet" href="css/nav.css"><link rel="stylesheet
 			</ul>\
     </div>');
 
-$(document).ready(function(){
-   $(window).on('resize' , function(){
-            resize('#about' , '#bio');
-   });
 
-});
-
-function resize(el1 , el2){
-    var $window = $(window);
-    if($window.width() <= 600){
-         $(el2).insertBefore(el1);
-    }else{
-         $(el2).insertAfter(el1);
-    }
-}
 
 function myFunction() {
   var x = document.getElementById("myLinks");
