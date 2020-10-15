b6aff8621497fd3678d04ccd36c8a5232a8ca7b9
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2020-10-15 15:05:24 -0500

    Updated colors and fixed navbar

--- nav.js
+++ nav.js
@@ -10,7 +10,7 @@ document.write('\<link rel="stylesheet" href="css/nav.css"><link rel="stylesheet
 				<li><a href="index.html">Home</a></li>\
         <li><a href="research-ux.html">Research</a></li>\
         <li><a href="design.html">Design + Development</a></li>\
-				<li><a href="resume.html">Résumé</a><li>\
+				<li><a href="cv.html">Résumé</a><li>\
 			</ul>\
     </div>');
 
