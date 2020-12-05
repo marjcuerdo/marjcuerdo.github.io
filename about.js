7331f1cea8b216d5df151f925a444622a72efb99
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2020-12-05 14:17:25 -0600

    changed news and twitter positions

--- about.js
+++ about.js
@@ -17,7 +17,15 @@ document.write('<section id="about" name="about">\
 					</div>\
 				</div>\
 			</div>\
-				<div class="twit">\
-					<a class="twitter-timeline" href="https://twitter.com/marj_cuerdo?ref_src=twsrc%5Etfw">Tweets by marj_cuerdo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\
+				<div class="news">\
+					<h3>Recent News</h3>\
+					<ul>\
+						<li><u class="month">March 2020.</u> I was accepted into the Ph.D. Computational Media program at UC Santa Cruz!</li><br>\
+						<li><u class="month">February 2020.</u> My first conference submissions were accepted! Prior to the pandemic, my first CHI conference would\'ve been in Honolulu, Hawaii to present my Late-Breaking Work (LBW) on a death and rebirth taxonomy for platformers. I was also part of another LBW on adapting the SUS towards children.</li><br>\
+						<li><u class="month">December 2019.</u> Got a publication date for my first book chapter with my advisor Professor Edward Melcer: “Death & Rebirth in Platformer Games” in the book <i><a href="https://www.springer.com/gp/book/9783030376420" target="_blank">"Game User Experience and Player-Centered Design"</i></a>. </li><br>\
+						<li><u class="month">June 2019.</u> This summer, I\'m in Chicago working as a research assistant for a project on child-computer interaction, and remotely on a research project about death and rebirth in platformers.\
+						</li><br>\
+						<li><u class="month">January 2019.</u> I\'m now in the <a href="https://www.cdm.depaul.edu/academics/Pages/current/Requirements-MS-in-Human-Computer-Interaction.aspx" target="_blank">M.S. in Human-Computer Interaction program</a> at DePaul University in the <a href="https://www.cdm.depaul.edu/Pages/default.aspx" target="_blank">College of Computing and Digital Media</a>. Excited to focus my program on UX research and design, learning, and video games.</li><br>\
+					</ul>\
 				</div>\
 		</section>');
