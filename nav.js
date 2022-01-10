b2cb8cea05b633ce7eaf3e22bd34a66126d8d280
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2022-01-10 11:23:31 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -8,7 +8,7 @@ document.write('\<link rel="stylesheet" href="css/nav.css"><link rel="stylesheet
 	    </div>\
 			<ul id="myLinks">\
 				<li><a href="index.html">Home</a></li>\
-        <li><a href="research-acad.html">Research</a></li>\
+        <li><a href="research-ux.html">Research</a></li>\
         <li><a href="design.html">Coding + Design</a></li>\
 				<li><a href="cv.html">Résumé</a><li>\
 			</ul>\
