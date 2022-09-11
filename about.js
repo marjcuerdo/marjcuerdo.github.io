2ad3a33d03e01b45c80786cd42e6129258bd27e8
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2022-09-11 16:11:00 -0500

    updated projects

--- about.js
+++ about.js
@@ -3,7 +3,7 @@ document.write('<section id="about" name="about">\
 				<div class="container">\
 					<div class="row">\
 							<div class="col-lg-6">\
-								<b><p><img id="pic" src="images/cuerdo.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
+								<b><p><img id="pic" src="images/marj.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
 							</div>\
 							<div class="col-lg-6">\
 								<p><a href="mailto:mcuerdo@ucsc.edu">mcuerdo@ucsc.edu</a><br></p>\
