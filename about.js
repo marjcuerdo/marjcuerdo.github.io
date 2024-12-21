363609c24c5d2c9032250e1d270ac8a8ce841680
Marjorie Ann Cuerdo <marjorie.cuerdo@gmail.com>
2024-12-21 15:11:04 -0600

    update acad

--- about.js
+++ about.js
@@ -6,12 +6,11 @@ document.write('<section id="about" name="about">\
 								<b><p><img id="pic" src="images/marj.jpg" alt="Picture of Marjorie" height="75%" width="75%" ></p>\
 							</div>\
 							<div class="col-lg-6">\
-								<p><a href="mailto:mcuerdo@ucsc.edu">mcuerdo@ucsc.edu</a><br></p>\
+								<p><a href="mailto:marjorie.cuerdo@gmail.com">marjorie.cuerdo@gmail.com</a><br></p>\
 							</div>\
 							<div id="links">\
 								<a href="https://scholar.google.com/citations?user=nvgtgq4AAAAJ&hl=en" target="_blank"><img src="images/scholar.jpeg"></a>\
 								<a href="https://www.linkedin.com/in/marj-cuerdo-12979277/" target="_blank"><img src="images/linkedin.png" ></a>\
-								<a href="https://twitter.com/marj_cuerdo" target="_blank"><img src="images/x.png"></a>\
 								<a href="https://www.researchgate.net/profile/Marjorie-Ann-Cuerdo" target="_blank"><img src="images/rg.png"></a>\
 							</div>\
 					</div>\
